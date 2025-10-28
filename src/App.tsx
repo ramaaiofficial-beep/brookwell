d3981eddc1ade15f9f23b97b140d408a806801cb
ramaaiofficial-beep <ramaai.official@gmail.com>
2025-10-28 19:49:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "src/pages/Index";
+import Index from "./pages/Index";
 import AboutUsPage from "src/pages/AboutUsPage";
 import ThrieBeamPage from "src/pages/ThrieBeamPage";
 import InquiryPage from "src/pages/InquiryPage";
